038a8c301528aebee34f11ea1261d04759534d8a
Sarthak Dixit <94391019+sarthakdixit2003@users.noreply.github.com>
2023-04-28 22:22:47 +0530

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,7 +4,7 @@ import { Box, Stack, Typography } from '@mui/material';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [selectedCategory, setSelectedCategory] = useState('Education');
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
